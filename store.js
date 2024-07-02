f81a4eeeb541f650331bddbcb979e5e7187f9415
saket-kr-jha <65586198+saket-kr-jha@users.noreply.github.com>
2024-07-02 21:10:48 +0530

    completed renderer

---
+++ store.js
@@ -0,0 +1,17 @@
+const store = {
+    todos: [
+      {
+        id: "1",
+        title: "Complete Task A",
+        completed: false,
+      },
+      {
+        id: "2",
+        title: "Read Book",
+        completed: true,
+      },
+    ],
+  };
+
+
+  export default store;
