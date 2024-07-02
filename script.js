af5c18c5bf2b97354436c9a563a3c80756c50792
saket-kr-jha <65586198+saket-kr-jha@users.noreply.github.com>
2024-07-02 21:48:08 +0530

    add todo

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 import render from "./render.js";
+import { addTodo } from "./store.js";
+
+window.addEventListener("todoschange", () =>{
+    render();
+})
 render()
 
+const form = document.querySelector("#form");
+const todoTitleInput = document.querySelector(".todo-title");
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const todoTitle = todoTitleInput.value;
+    const newTodo = {id: crypto.randomUUID(), title:todoTitle , completed:false}
+    console.log( todoTitle);
+    addTodo(newTodo);
+})
--- store.js
+++ store.js
@@ -13,5 +13,26 @@ const store = {
     ],
   };
 
+  const storeHandler = {
+    get(target, property){
+        console.log("Oh you are trying to get ", property)
+        return target[property]
+    },
 
-  export default store;
+    set(target, property, value){
+        target[property]= value;
+        if (property == "todos") {
+            window.dispatchEvent(new Event("todoschange"));
+          }
+          return true;
+    },
+  }
+
+  const storeProxy = new Proxy(store, storeHandler)
+
+  function addTodo(newTodo){
+    storeProxy.todos = [...storeProxy.todos, newTodo];
+  }
+
+  export default storeProxy;
+  export {addTodo};
